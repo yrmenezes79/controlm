f2832cb01dcca1e42a02f5562551c6f206f01488
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-10-30 16:21:41 -0300

    Create delete_file.py

---
+++ Projeto13/delete_file.py
@@ -0,0 +1,16 @@
+import glob
+import os
+from datetime import datetime
+
+def delete_files(jobname):
+    # Adiciona "**" ao final do jobname para buscar arquivos que começam com o nome
+    files_to_delete = glob.glob(f"{jobname}**")
+    print(f"{datetime.now().strftime('%d-%m-%Y_%H:%M:%S')} Delecao de arquivos {jobname}")
+
+    # Itera sobre a lista de arquivos e os deleta
+    for file_path in files_to_delete:
+        try:
+            os.remove(file_path)
+            print(f"{datetime.now().strftime('%d-%m-%Y_%H:%M:%S')} {file_path} deletado com sucesso")
+        except Exception as e:
+            print(f"{datetime.now().strftime('%d-%m-%Y_%H:%M:%S')} Erro ao deletar {file_path}: {e}")
